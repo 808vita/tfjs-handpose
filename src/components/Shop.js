d099f87cbb7dfcdea4212f8b94bc81f4dcdb2d8c
808vita <its808vita@gmail.com>
2023-10-09 16:15:02 +0530

    shop stage states

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,7 +4,7 @@ import {
   SupportedModels,
 } from "@tensorflow-models/hand-pose-detection";
 import "@tensorflow/tfjs-backend-webgl";
-import { drawHands ,drawFaces} from "@/utils/new-utils";
+import { drawHands, drawFaces } from "@/utils/new-utils";
 
 import { useAnimationFrame } from "@/hooks/useAnimationFrame";
 import * as tfjsWasm from "@tensorflow/tfjs-backend-wasm";
@@ -12,9 +12,8 @@ import * as tfjsWasm from "@tensorflow/tfjs-backend-wasm";
 tfjsWasm.setWasmPaths(
   `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm`
 );
-import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
-import * as faceMesh from '@mediapipe/face_mesh';
-
+import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
+import * as faceMesh from "@mediapipe/face_mesh";
 
 import * as fp from "fingerpose";
 
@@ -57,15 +56,14 @@ async function setupDetector() {
 async function setupFaceDetector() {
   const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
   const detector = await faceLandmarksDetection.createDetector(model, {
-      runtime: 'mediapipe',
-      solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@${faceMesh.VERSION}`,
-      maxFaces: 2,
-      refineLandmarks: true
+    runtime: "mediapipe",
+    solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@${faceMesh.VERSION}`,
+    maxFaces: 2,
+    refineLandmarks: true,
   });
 
   return detector;
 }
-
 
 async function setupCanvas(video) {
   const canvas = document.getElementById("canvas");
@@ -77,12 +75,25 @@ async function setupCanvas(video) {
   return ctx;
 }
 
+const shopStage = {
+  start: "start",
+  checkout: "checkout",
+  confirm: "confirmed",
+  cancelled: "cancelled",
+};
+
+const gesturesList = ["ok", "thumbs_up", "thumbs_down"];
+
 export default function Shop() {
   const detectorRef = useRef();
   const detectorFaceRef = useRef();
   const videoRef = useRef();
   const [ctx, setCtx] = useState();
-  const contours = faceLandmarksDetection.util.getKeypointIndexByContour(faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh);
+  const contours = faceLandmarksDetection.util.getKeypointIndexByContour(
+    faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh
+  );
+  const [gesture, setGesture] = useState("start");
+  const [shopStage, setShopStage] = useState("start");
 
   useEffect(() => {
     async function initialize() {
@@ -102,7 +113,7 @@ export default function Shop() {
       flipHorizontal: false,
     });
     const faces = await detectorFaceRef.current.estimateFaces(videoRef.current);
-    
+
     ctx.clearRect(
       0,
       0,
@@ -123,21 +134,48 @@ export default function Shop() {
       console.log(hands?.length);
       const GE = new fp.GestureEstimator([
         // fp.Gestures.VictoryGesture,
-        fp.Gestures.ThumbsUpGesture,
+        fp.Gestures.ThumbsUpGesture, //ignoring this
         okGesture,
         thumbsDown,
       ]);
-    
 
       let landmarks = createLandmarks(hands[0]);
 
-      const gesture = await GE.estimate(landmarks, 7);
+      const gesture = await GE.estimate(landmarks, 6);
 
       console.log(gesture);
       if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
         console.log(gesture.gestures);
         console.log(bestMatch(gesture.gestures));
+        const getGesture = bestMatch(gesture.gestures)?.name;
+        console.log(getGesture);
 
+        if (gesturesList.includes(getGesture)) {
+          setGesture((prev) => getGesture);
+
+          setShopStage((prev) => {
+            if (prev === "start" && getGesture === "thumbs_up") {
+              return "checkout";
+            } else if (prev === "checkout" && getGesture === "ok") {
+              const resetShop = setTimeout(() => {
+                clearTimeout(resetShop);
+                console.log("oof");
+                setShopStage(() => "start");
+              }, 10000);
+
+              return "confirm";
+            } else if (prev === "checkout" && getGesture === "thumbs_down") {
+              const resetShop = setTimeout(() => {
+                clearTimeout(resetShop);
+                console.log("oof");
+                setShopStage(() => "start");
+              }, 10000);
+
+              return "cancelled";
+            }
+            return prev;
+          });
+        }
       }
       //
     }
@@ -147,15 +185,14 @@ export default function Shop() {
   }, !!(detectorRef.current && detectorFaceRef.current && videoRef.current && ctx));
 
   return (
-    <div>
-      <main>
+    <div className="grid grid-cols-2 gap-2">
+      <div>
         <canvas
           style={{
             transform: "scaleX(-1)",
             zIndex: 1,
-            borderRadius: "1rem",
             boxShadow: "0 3px 10px rgb(0 0 0)",
-            maxWidth: "85vw",
+            // maxWidth: "85vw",
           }}
           id="canvas"
         ></canvas>
@@ -172,7 +209,43 @@ export default function Shop() {
           id="video"
           playsInline
         ></video>
-      </main>
+      </div>
+      <div className="bg-zinc-200">
+        <h1 className="text-xl text-fuchsia-700">{gesture}</h1>
+        <h1 className="text-xl text-fuchsia-700">{shopStage}</h1>
+
+        {shopStage === "start" && (
+          <div>
+            <h2>Welcome Cat Food Shop</h2>
+            <h2>Show "Thumbs Up" to select cat food </h2>
+          </div>
+        )}
+
+        {shopStage === "checkout" && (
+          <div>
+            <h2>Checkout Stage</h2>
+            <h2>Show "OK" to place an order </h2>
+            <h2>OR</h2>
+            <h2>Show "Thumbs Down" to cancel order </h2>
+          </div>
+        )}
+
+        {shopStage === "confirm" && (
+          <div>
+            <h2>Order Placed</h2>
+            <h2>Thanks for placing an order</h2>
+            <h2>Shop will reset in 10 seconds</h2>
+          </div>
+        )}
+
+        {shopStage === "cancelled" && (
+          <div>
+            <h2>Order Cancelled</h2>
+            <h2>show zoned cat gif</h2>
+            <h2>oof,shop will reset in 10 seconds</h2>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
